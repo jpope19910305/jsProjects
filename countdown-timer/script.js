609a1033a0217dede14af0d84c91016daea4b450
jpope19910305 <jptechservices91@gmail.com>
2020-11-18 15:14:29 -0500

    changes made

--- countdown-timer/script.js
+++ countdown-timer/script.js
@@ -1,4 +1,10 @@
-const newYears = '5 Mar2021';
+const daysEl =document.getElementById('days');
+const hoursEl =document.getElementById('hours');
+const minEl =document.getElementById('min');
+const secondsEl =document.getElementById('seconds');
+
+
+  const newYears = '5 Mar 2021';
 
  function countdown() {
 const newYearsDate = new Date(newYears);
@@ -8,11 +14,20 @@ const totalSeconds = new Date(newYearsDate - currentDate) / 1000;
 
 const days = Math.floor(totalSeconds / 3600 / 24);
 const hours = Math.floor(totalSeconds / 3600) % 24;
-const minutes = Math.floor(totalSeconds / 60 ) % 60;
+const min = Math.floor(totalSeconds / 60 ) % 60;
 const seconds = Math.floor(totalSeconds) % 60;
-console.log(days, hours, minutes, seconds);
 
+daysEl.innerHTML = days;
+hoursEl.innerHTML = formatTime(hours);
+minEl.innerHTML = formatTime(min);
+secondsEl.innerHTML = formatTime(seconds);
  }
+
+function formatTime(time) {
+    return time < 10 ? (`0${time}`) : time;
+}
+
+
 
 countdown();
 
